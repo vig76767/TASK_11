a6e07dc8ba638ddcb835e7777e0babd5c79502b4
vig76767 <153997956+vig76767@users.noreply.github.com>
2025-05-22 16:06:49 +0530

    Create imple .java

---
+++ imple .java
@@ -0,0 +1,10 @@
+package com.example.guvipeoject11.Service;
+
+import com.example.guvipeoject11.Entity.Product;
+
+import java.util.List;
+
+public interface imple {
+    Product addproduct(Product product);
+    List<Product> view();
+}
