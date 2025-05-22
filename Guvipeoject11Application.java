f5d30e155cd3e9196c621db2e958bea79d33d564
vig76767 <153997956+vig76767@users.noreply.github.com>
2025-05-22 15:57:13 +0530

    Create Guvipeoject11Application.java

---
+++ Guvipeoject11Application.java
@@ -0,0 +1,13 @@
+package com.example.guvipeoject11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Guvipeoject11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Guvipeoject11Application.class, args);
+	}
+
+}
